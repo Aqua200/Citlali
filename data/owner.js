84de234c561bf938a844094ca3aca3565401c88f
Neykoor💜 <chinquepapa@gmail.com>
2025-07-09 02:01:06 -0700

    Delete data/owner.js

--- data/owner.js
+++
@@ -1,9 +0,0 @@
-// data/owner.js
-
-// Lista de propietarios del bot.
-// Formato: [{id: 'numero', name: 'Nombre Visible', isPrimary: true/false}]
-// ¡IMPORTANTE!: Los números deben ir SIN el "@s.whatsapp.net"
-export const owners = [
-  { id: '5216631079388', name: 'Neykoor (Citlali Prime)', isPrimary: true }, // Tu número principal: +52 1 663 107 9388
-  { id: '117094280605916', name: 'Destroy (Dev)', isPrimary: false }
-];
