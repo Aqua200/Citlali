cb333b4670f0cde60af00c98dc5d4b4db8fb0f51
Neykoor💜 <chinquepapa@gmail.com>
2025-07-09 02:01:39 -0700

    Update and rename community.js to config.js

---
+++ data/config.js
@@ -0,0 +1,57 @@
+
+import { readFileSync } from 'fs'
+
+// --- Propietarios y Roles ---
+export const owner = [
+  ['5216631079388', '🜲 Propietario 🜲', true],
+  ['5212202410659'],
+  ['573154062343'],
+  ['573214401313'], 
+  [''],
+  ['117094280605916', 'Destroy', true],
+  ['5216631079388', 'Neykoor', true]
+];
+
+export const mods = []
+export const suittag = [] 
+export const prems = []
+
+// --- Información del Bot ---
+export const nameqr = 'Citlali-MD'
+export const namebot = 'Citlali'
+export const sessions = 'Sessions'
+export const jadi = 'JadiBots' 
+export const botNumber = '' // Deja vacío si no quieres preestablecer, Ej: '573218138672'
+
+// --- Nombres y Textos ---
+export const packname = '˚୨୧⋆｡Citlali˚୨୧⋆｡'
+export const botname = '˚୨୧⋆｡Citlali˚୨୧⋆｡'
+export const wm = '⋆｡ Citlali ｡⋆'
+export const author = '© 𝚖𝚊𝚍𝚎 𝚠𝚒𝚝𝚑 𝚋𝚢 ⁱᵃᵐ|Neykoor'
+export const dev = '© ⍴᥆ᥕᥱrᥱძ 𝚋𝚢 ⁱᵃᵐ|Neykoor'
+export const textbot = 'Citlali, 𝚖𝚊𝚍𝚎 𝚠𝚒𝚝𝚑 𝚋𝚢 ⁱᵃ𝚖|Neykoor'
+export const etiqueta = 'ⁱᵃᵐ|Neykoor'
+
+// --- Moneda y Mensajes de Bienvenida ---
+export const moneda = '¥enes'
+export const welcom1 = '❍ Edita Con El Comando setwelcome'
+export const welcom2 = '❍ Edita Con El Comando setbye'
+
+// --- Enlaces y Contacto ---
+export const banner = 'https://files.catbox.moe/e98ktt.jpg'
+export const avatar = 'https://files.catbox.moe/oly31v.jpeg'
+export const gp1 = 'https://chat.whatsapp.com/Gqv0byeAjXiHPL5bX94UGE'
+export const comunidad1 = 'https://chat.whatsapp.com/CHXQizfRDItFdWM8F217oB'
+export const channel = 'https://whatsapp.com/channel/0029VazHywx0rGiUAYluYB24'
+export const channel2 = 'https://whatsapp.com/channel/0029VazHywx0rGiUAYluYB24'
+export const md = 'https://github.com/Aqua200'
+export const correo = 'chinquepapa@gmail.com'
+
+// --- Otros Ajustes ---
+export const catalogo = readFileSync('./src/catalogo.jpg'); // Asegúrate de que este archivo exista
+export const estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: packname, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
+export const ch = {
+ch1: '120363392571425662@newsletter',
+}
+export const multiplier = 60
+export const CitlaliJadibts = true // Renombrado de FurinaJadibts a CitlaliJadibts
