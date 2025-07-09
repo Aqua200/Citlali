c7b57491dba80317313e746cac5ba0247cd4509b
Neykoor💜 <chinquepapa@gmail.com>
2025-07-09 01:19:30 -0700

    Create community.js

---
+++ data/community.js
@@ -0,0 +1,9 @@
+// Enlaces a grupos, canales y otros recursos de la comunidad.
+export const communityLinks = {
+  mainGroup: 'https://chat.whatsapp.com/Gqv0byeAjXiHPL5bX94UGE',
+  supportGroup: 'https://chat.whatsapp.com/CHXQizfRDItFdWM8F217oB',
+  officialChannel: 'https://whatsapp.com/channel/0029VazHywx0rGiUAYluYB24',
+  secondaryChannel: 'https://whatsapp.com/channel/0029VazHywx0rGiUAYluYB24',
+  documentation: 'https://github.com/TuUsuario/TuRepositorioCitlali/wiki',
+  faqPage: 'https://tusitioweb.com/faq',
+};
