274b85cd34e56afb1f44f28a6a6fee207eebb506
Neykoor💜 <chinquepapa@gmail.com>
2025-07-09 02:10:36 -0700

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ import fs, {readdirSync, statSync, unlinkSync, existsSync, mkdirSync, readFileSy
 import yargs from 'yargs';
 import {spawn} from 'child_process'
 import lodash from 'lodash'
-// ¡CORREGIDO AQUÍ! Ahora se importa como CitlaliJadiBot
-import { FurinaJadiBot as CitlaliJadiBot } from './plugins/jadibot-serbot.js' 
+import { CitlaliJadiBot as CitlaliJadiBot } from './plugins/jadibot-serbot.js' 
 import chalk from 'chalk'
 import syntaxerror from 'syntax-error'
 import {tmpdir} from 'os'
